2cfa08f6010d2fec517b11e711fef0b59b889cad
jenny xu <jingwenxu13@gmail.com>
2025-04-01 17:38:03 -0700

    implements wave123&tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+### Wave 2
+
+# In Wave 2 we will create the `Item` class and the `Vendor` class' `get_by_id` method.
+
+
+from uuid import uuid4
 class Item:
-    pass
+    def __init__(self, id = None):
+        self.id = id if id is not None else uuid4().int
+    
+    def get_category(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
