250014936892be1d504f8cbe5565a903350cc805
Gitika Kalal <gitikakalal21@gmail.com>
2025-04-01 17:46:16 -0700

    completed wave1,2and3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        self.id = uuid.uuid4().int if id is None else id
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+
+    def get_category(self):
+        # return "Item"
+        return self.__class__.__name__ 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,30 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = [] if inventory is None else inventory
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, removed_item):
+        if removed_item not in self.inventory:
+            return False
+        self.inventory.remove(removed_item)
+        return removed_item
+    
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.inventory.remove(my_item)
+        other_vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
+        other_vendor.inventory.append(my_item)
+        return True
+
+
